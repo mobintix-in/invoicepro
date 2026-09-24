cfb5dbfbd8b4f98ebaf2d08436e9e7cf0993a730
Aryan Bhimani <aryan.bhimani.93@gmail.com>
2026-09-24 21:59:07 +0530

    Mobintix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/welcome',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
